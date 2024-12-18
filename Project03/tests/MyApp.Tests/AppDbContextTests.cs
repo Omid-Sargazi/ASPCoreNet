2ef0ae1090db61f38ecb95469c6055733359b418
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-19 00:26:09 +0330

    Add a Product to the Database

--- Project03/tests/MyApp.Tests/AppDbContextTests.cs
+++ Project03/tests/MyApp.Tests/AppDbContextTests.cs
@@ -14,18 +14,58 @@ namespace MyApp.Tests
     public void CanInsertProductIntoDatabase()
     {
         var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDatabase")
-            .Options;
+        .UseInMemoryDatabase(databaseName:"TestDatabase").Options;
 
         using (var context = new AppDbContext(options))
         {
-            var product = new Product { Name = "Test Product", Price = 9.99m };
+            var product = new Product{Name="Test",Price=99.9m};
             context.Products.Add(product);
             context.SaveChanges();
 
-            Assert.Equal(1, context.Products.Count());
-            Assert.Equal("Test Product", context.Products.Single().Name);
+
+            Assert.Equal(1,context.Products.Count());
+            Assert.Equal("Test",context.Products.Single().Name);
         }
     }
+    [Fact]
+    public void DatabaseStartsEmpty()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+        .UseInMemoryDatabase(databaseName:"EmptyDatabaseTest")
+        .Options;
+
+        using(var context = new AppDbContext(options))
+        {
+            var product = new Product{Name="laptop", Price=99.3m};
+            context.Products.Add(product);
+            context.SaveChanges();
+            
+
+            Assert.Empty(context.Products);
+        }
+    }
+
+
+    [Fact]
+    public void CanAddProduct()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+        .UseInMemoryDatabase(databaseName:"AddProductTest").Options;
+
+        using(var context = new AppDbContext(options))
+        {
+            var product = new Product{Name="Laptop",Price=1500m};
+            var product2 = new Product{Name="PC",Price=1600m};
+            context.Products.Add(product);
+            context.Products.Add(product2);
+            context.SaveChanges();
+
+            Assert.Single(context.Products);
+            Assert.Equal("Laptop",context.Products.Single().Name);
+
+        }
+    }
+
+    
 }
 }
