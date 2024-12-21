c25a8769fc06622d3e594f7eb3ebefdc9ba41057
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-21 18:02:54 +0330

    class without DI

---
+++ Project03/DIAndIOC/DIAndIOC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DIAndIOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ProductService p1 = new ProductService();
+            p1.AddProduct("Salam.....");
+        }
+    }
+
+
+    public class ProductService
+    {
+        private Logger _logger;
+
+        public ProductService()
+        {
+            _logger=new Logger();
+        }
+
+        public void AddProduct(string product)
+        {
+            _logger.Log($"Product {product} added.");
+        }
+    }
+
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
