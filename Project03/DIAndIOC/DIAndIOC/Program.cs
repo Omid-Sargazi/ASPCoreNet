27e4a4936c1b3621353eb0b20ec4227abd1666ba
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-21 19:27:16 +0330

    NotificationService

--- Project03/DIAndIOC/DIAndIOC/Program.cs
+++ Project03/DIAndIOC/DIAndIOC/Program.cs
@@ -179,4 +179,96 @@ namespace DIAndIOC
         }
     }
 
+
+    public class NotificationService
+    {
+        private EmailService _emailService;
+
+        public NotificationService()
+        {
+            _emailService=new EmailService();
+        }
+
+        public void Notify(string message)
+        {
+            _emailService.SendEmail(message);
+        }
+    }
+
+
+    public class EmailService
+    {
+        public void SendEmail(string message)
+        {
+            Console.WriteLine($"Email sent: {message}");
+        }
+    }
+
+
+    public class NotificationServiceDI
+    {
+        private readonly IEmailService _emailService;
+        
+        public NotificationServiceDI(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public void Notify(string message)
+        {
+            _emailService.SendEmail(message);
+        }
+    }
+
+    public interface IEmailService
+    {
+        void SendEmail(string message);
+    }
+
+    public class EmailServiceDI:IEmailService
+    {
+        public void SendEmail(string message)
+        {
+            Console.WriteLine($"Email sent: {message}");
+        }
+    }
+
+
+    public interface INotificationService
+    {
+        public void SendNotification();
+    }
+
+    public class EmailNotification:INotificationService
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("Email notification sent.");
+        }
+    }
+
+    public class SmsNotification:INotificationService
+    {
+        public void SendNotification()
+        {
+            Console.WriteLine("SMS notification sent.");
+        }
+    }
+
+
+    public class NotificationManager
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationManager(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public void SendNotification()
+        {
+            _notificationService.SendNotification();
+        }
+    }
+
 }
