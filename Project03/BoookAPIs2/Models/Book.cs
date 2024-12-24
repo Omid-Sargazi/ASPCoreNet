89ef8551b595c154ff5e1edee6c53cf14e5994fb
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-24 23:52:51 +0330

    defien a model with all relations

--- Project03/BoookAPIs2/Models/Book.cs
+++ Project03/BoookAPIs2/Models/Book.cs
@@ -7,6 +7,12 @@ namespace BoookAPIs2.Models
 {
     public class Book
     {
-        
+        public int Id {get;set;}
+        public string Title {get;set;}
+        public int AuthorId {get;set;}
+        public int GenreId {get;set;}
+
+        public Author Author {get;set;}
+        public Genre Genre {get;set;}
     }
 }
