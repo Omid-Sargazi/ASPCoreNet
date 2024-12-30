fbfffca281c42ae7782104810bc0527db73c6fbf
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-30 20:36:37 +0330

    Registering and resolving dependencies through a DI container

--- Project05/DependancyInjection/Program.cs
+++ Project05/DependancyInjection/Program.cs
@@ -1,5 +1,7 @@
 ﻿using DependancyInjection.Implements;
+using DependancyInjection.Interfaces;
 using DependancyInjection.MainClasses;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Program
 {
@@ -7,19 +9,29 @@ public class Program
     {
         Console.WriteLine("Hello");
 
-        var Logger = new ConsoleLogger();
-        var Service = new Service(Logger);
-        Service.PerformTask();
+        // var Logger = new ConsoleLogger();
+        // var Service = new Service(Logger);
+        // Service.PerformTask();
         
-        //Next
-        var service02 = new Service02()
-        {
-            logger = new ConsoleLogger()
-        };
-        service02.PerformTask();
-        //Next:Injecting a dependency through a method parameter.
+        // //Next
+        // var service02 = new Service02()
+        // {
+        //     logger = new ConsoleLogger()
+        // };
+        // service02.PerformTask();
+        // //Next:Injecting a dependency through a method parameter.
 
-        var service03 = new Service03();
-        service03.PerformTask(new ConsoleLogger());
+        // var service03 = new Service03();
+        // service03.PerformTask(new ConsoleLogger());
+        // //Using a DI Container 
+
+        var services = new ServiceCollection();
+        services.AddTransient<ILogger, ConsoleLogger>();
+        services.AddTransient<Service>();
+
+        var provider = services.BuildServiceProvider();
+
+        var service04 = provider.GetService<Service>();
+        service04.PerformTask();
     }
 }
