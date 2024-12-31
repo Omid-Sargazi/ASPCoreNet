eb6a9feb261674f17b16e51a8da0d7c5b23a2f95
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-31 20:37:04 +0330

    Configure DI and Database Initialization

--- Project05/OrderManagement/Program.cs
+++ Project05/OrderManagement/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Data;
+using OrderManagement.Implements;
+using OrderManagement.Repositories;
+using OrderManagement.Services;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<OrderDbContext>();
+
+services.AddScoped<IOrderRepository,OrderRepository>();
+
+services.AddScoped<IOrderService,OrderService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+
+using(var scope = serviceProvider.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+
+var orderService = serviceProvider.GetRequiredService<IOrderService>();
+
+
+await orderService.AddOrderAsync("John Doe", DateTime.Now, 150.50m);
+await orderService.AddOrderAsync("Jane Smith", DateTime.Now.AddDays(-1), 300.00m);
+
+var orders = await orderService.GetOrdersAsync();
+foreach (var order in orders)
+{
+    Console.WriteLine($"ID: {order.Id}, Customer: {order.CustomerName}, Date: {order.OrderDate}, Total: {order.TotalAmount}");
+}
