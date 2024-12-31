d963b6b71f37de77742bdbe71bcca32de0e2f27f
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-31 18:26:45 +0330

    Set Up the DbContext Create a AppDbContext class for SQLite configuration

--- Project05/RepositoryPatternExample/Data/AppDbContext.cs
+++ Project05/RepositoryPatternExample/Data/AppDbContext.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RepositoryPatternExample.models;
 
 namespace RepositoryPatternExample.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
-        
+        DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=product.db");
+        }
     }
 }
