e3a993273c9dc579c1967fd796f2f1d322b20eb6
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-31 19:06:06 +0330

    Configure DI and Database Initialization

--- Project05/RepositoryPatternExample/Program.cs
+++ Project05/RepositoryPatternExample/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using RepositoryPatternExample.Data;
+using RepositoryPatternExample.Implements;
+using RepositoryPatternExample.Interfaces;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<AppDbContext>();
+
+services.AddScoped<IProductRepository,ProductRepository>();
+
+services.AddScoped<IProductService,ProductService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+using(var scope = serviceProvider.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
+}
+
+
+var productService = serviceProvider.GetRequiredService<IProductService>();
+
+await productService.AddProductAsync("Product 1", 100);
+await productService.AddProductAsync("Product 2", 200);
+
+var products = await productService.GetProductsAsync();
+foreach (var product in products)
+{
+    Console.WriteLine($"ID: {product.Id}, Name: {product.Name}, Price: {product.Price}");
+}
