5308bc910e678ccd647f8e15c57e2d0bbbcc44ca
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-02 14:06:35 +0330

    StringExtensions

---
+++ Project05/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Omid");
+        string example = null;
+
+        bool result = example.IsNullOrEmpty();
+        Console.WriteLine(result);
+    }
+}
+
+public static class StringExtensions
+{
+    public static bool IsNullOrEmpty(this string str)
+    {
+        return string.IsNullOrEmpty(str);
+    }
+
+
+}
+
+
