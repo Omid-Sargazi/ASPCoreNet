001102a5aceeb40edcc919126434a1507647e422
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-02 21:32:38 +0330

    Use the Extension in the Service Layer

--- Project05/ExtensionMethods02/Program.cs
+++ Project05/ExtensionMethods02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExtensionMethods02.Services;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var service = new UserService();
+        var filteredUsers = service.GetFilteredUsers("Alice", 25, 30);
+
+
+         foreach (var user in filteredUsers)
+        {
+            Console.WriteLine($"{user.Name}, {user.Age}, {user.Email}");
+        }
+    }
+}
---
+++ Project05/ExtensionMethods02/Services/UserService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using ExtensionMethods02.Main;
+using ExtensionMethods02.Models;
+
+namespace ExtensionMethods02.Services
+{
+    public class UserService
+    {
+        private readonly List<User> _users;
+
+        public UserService()
+        {
+            _users = new List<User>
+            {
+                new User { Id = 1, Name = "Alice", Age = 25, Email = "alice@example.com" },
+                new User { Id = 2, Name = "Bob", Age = 30, Email = "bob@example.com" },
+                new User { Id = 3, Name = "Charlie", Age = 35, Email = "charlie@example.com" }
+            };
+        }
+
+        public IEnumerable<User> GetFilteredUsers(string name, int? minAge, int? maxAge)
+        {
+            return _users.AsQueryable().ApplyFilters(name, minAge, maxAge).ToList();
+        }
+    }
+
+}
