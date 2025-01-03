b466aba4146e13a8805fff599e53b4d3325d2a6a
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-03 18:14:10 +0330

    GetEndpoint

--- Project06/RoutingExample/Program.cs
+++ Project06/RoutingExample/Program.cs
@@ -1,11 +1,50 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseRouting();//Active 
-app.UseEndpoints(endpoints=>{
-    //exceute endpoint.
+app.Use(async(HttpContext context, RequestDelegate next) =>{
+    Microsoft.AspNetCore.Http.Endpoint? endPoint = context.GetEndpoint();
+    //Console.WriteLine($"Endpoint Name: {endPoint.DisplayName}");
+    await next(context);
 });
 
-// app.MapGet("/", () => "Hello World!");
+app.UseRouting();//Active 
+
+app.Use(async (HttpContext context, RequestDelegate next) =>{
+    Microsoft.AspNetCore.Http.Endpoint? endpoint=  context.GetEndpoint();
+    if(endpoint !=null)
+    {
+
+    Console.WriteLine($"Endpoint Name: {endpoint.DisplayName}");
+    await context.Response.WriteAsync($"Endpoint Name: {endpoint.DisplayName}"); 
+    }
+
+    await next(context);
+});
+
+
+
+app.UseEndpoints(endpoints=>{
+    //exceute endpoint.
+    endpoints.Map("/about", async (HttpContext context)=>{
+        await context.Response.WriteAsync("About Us");
+    });
+
+    endpoints.MapPost("/contact",async (context)=>{
+        await context.Response.WriteAsync("Contact Us");
+    });
+
+});
+
+app.UseEndpoints(endpoints=>{
+    endpoints.MapGet("/Home",async context=>{
+        await context.Response.WriteAsync("Home Page");
+    });
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context)=>{
+    await context.Response.WriteAsync($"You are in Path:  {context.Request.Path}");
+});
 
 app.Run();
