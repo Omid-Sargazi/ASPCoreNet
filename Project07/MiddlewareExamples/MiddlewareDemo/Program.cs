465e48a60e4c9e15b1cd995784bcf86de3a6ce72
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-04 22:51:37 +0330

    Response Time Middleware

--- Project07/MiddlewareExamples/MiddlewareDemo/Program.cs
+++ Project07/MiddlewareExamples/MiddlewareDemo/Program.cs
@@ -3,37 +3,44 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
+// app.Use(async(context,next)=>{
+//     Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+//     await next();
+//     Console.WriteLine($"Response: {context.Response.StatusCode}");
+// });
+
+
+// app.Use(async(context,next)=>{
+//     if(context.Request.Headers.ContainsKey("Authorization"))
+//     {
+//         context.Response.StatusCode = 401;
+//         await context.Response.WriteAsync("Unauthorized");
+//         await next();
+//         // return;
+//     }
+//     await next();
+// });
+
+
+
+
+
+// app.Use(async(context, next)=>{
+//     try{
+//         await next();
+//     }
+//     catch(Exception ex)
+//     {
+//          context.Response.StatusCode = 500;
+//         await context.Response.WriteAsync($"Error: {ex.Message}");
+//     }
+// });
+
 app.Use(async(context,next)=>{
-    Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+    var start = DateTime.Now;
     await next();
-    Console.WriteLine($"Response: {context.Response.StatusCode}");
-});
-
-
-app.Use(async(context,next)=>{
-    if(context.Request.Headers.ContainsKey("Authorization"))
-    {
-        context.Response.StatusCode = 401;
-        await context.Response.WriteAsync("Unauthorized");
-        await next();
-        // return;
-    }
-    await next();
-});
-
-
-
-
-
-app.Use(async(context, next)=>{
-    try{
-        await next();
-    }
-    catch(Exception ex)
-    {
-         context.Response.StatusCode = 500;
-        await context.Response.WriteAsync($"Error: {ex.Message}");
-    }
+    var processingTime = DateTime.UtcNow - start;
+    context.Response.Headers.Add("X-Processing-Time", processingTime.TotalMilliseconds.ToString());
 });
 
 
@@ -54,8 +61,6 @@ app.Use(async(context, next)=>{
 
 
 
-
-
-app.MapGet("/", () => "Logging Middleware!");
+app.MapGet("/", () => "Check the response headers for processing time!");
 
 app.Run();
--- Project07/MiddlewareExamples/MiddlewareDemo/obj/Debug/net9.0/MiddlewareDemo.AssemblyInfo.cs
+++ Project07/MiddlewareExamples/MiddlewareDemo/obj/Debug/net9.0/MiddlewareDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiddlewareDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+100a4fd2ca325d6353c5d9fa6901a38d98503269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41238a57b666c0e456b069b82f1ac56fc9bff586")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiddlewareDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiddlewareDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
