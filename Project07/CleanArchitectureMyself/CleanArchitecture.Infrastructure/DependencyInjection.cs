c5e2bab0682fe02815c348bdb26f3ddac31fa301
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-05 20:12:56 +0330

    define and use injection and extention method

---
+++ Project07/CleanArchitectureMyself/CleanArchitecture.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IWeatherForecastService,WeatherForecastService>();
+            return services;
+        }
+    }
+}
