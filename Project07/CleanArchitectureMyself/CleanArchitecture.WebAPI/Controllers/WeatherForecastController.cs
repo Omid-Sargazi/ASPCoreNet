c5e63ebde37844a1ef4881900891ef041c769a53
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-05 20:44:04 +0330

    Apply Validation in Controller

--- Project07/CleanArchitectureMyself/CleanArchitecture.WebAPI/Controllers/WeatherForecastController.cs
+++ Project07/CleanArchitectureMyself/CleanArchitecture.WebAPI/Controllers/WeatherForecastController.cs
@@ -4,25 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using CleanArchitecture.Application.Common.Interfaces;
 using CleanArchitecture.Application.WeatherForecasts.Queries;
+using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchitecture.WebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController
+    public class WeatherForecastController:ControllerBase
     {
         private readonly IWeatherForecastService _service;
+        private readonly IValidator<WeatherForecastDto> _validator;
 
-        public WeatherForecastController(IWeatherForecastService service)
+        public WeatherForecastController(IWeatherForecastService service, IValidator<WeatherForecastDto> validator)
         {
             _service = service;
+            _validator = validator;
         }
 
-        [HttpGet]
+       /* [HttpGet]
         public IEnumerable<WeatherForecastDto> Get()
         {
             return _service.GetWeatherForecasts();
+        }*/
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var forecasts = _service.GetWeatherForecasts();
+
+            foreach(var forecast in forecasts)
+            {
+                var validationResult = _validator.Validate(forecast);
+                if(!validationResult.IsValid)
+                {
+                    return BadRequest(validationResult.Errors);
+                }
+            }
+            return Ok(forecasts);
         }
     }
 }
