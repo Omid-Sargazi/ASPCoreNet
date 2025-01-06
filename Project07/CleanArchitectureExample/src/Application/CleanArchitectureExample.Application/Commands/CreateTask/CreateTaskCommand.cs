f913023954927d837420ce4523f82272c2ea979e
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-06 10:41:45 +0330

    A command to create a new task

--- Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/Commands/CreateTask/CreateTaskCommand.cs
+++ Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/Commands/CreateTask/CreateTaskCommand.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 
 namespace CleanArchitectureExample.Application.Commands.CreateTask
 {
-    public class CreateTaskCommand
+    public class CreateTaskCommand:IRequest<Guid>
     {
-        
+        public string Title {get;set;} = string.Empty;
+        public string Description {get;set;} = string.Empty;
+         public DateTime? DueDate { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProjectId { get; set; } 
     }
 }
