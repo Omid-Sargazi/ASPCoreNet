b23b86ffe0a9ae10e2c0e35ee6815f2a1c040ce7
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-06 10:46:25 +0330

    Handles the CreateTaskCommand

--- Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/Commands/CreateTask/CreateTaskCommandHandler.cs
+++ Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/Commands/CreateTask/CreateTaskCommandHandler.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchitectureExample.Domain.Entities;
+using CleanArchitectureExample.Domain.Interfaces;
+using MediatR;
 
 namespace CleanArchitectureExample.Application.Commands.CreateTask
 {
-    public class CreateTaskCommandHandler
+    public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand, Guid>
     {
-        
+        private readonly IRepository<TaskItem> _repository;
+
+        public CreateTaskCommandHandler(IRepository<TaskItem> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<Guid> Handle(CreateTaskCommand request, CancellationToken cancellationToken)
+        {
+            var task = new TaskItem 
+            {
+                Title = request.Title,
+                Description = request.Description,
+                DueDate = request.DueDate,
+                UserId = request.UserId,
+                ProjectId = request.ProjectId,
+            };
+            await _repository.AddAsync(task);
+            return task.Id;
+        }
     }
 }
