a1adc20829f1da394235003b9543cb893cd7fad8
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-06 13:04:28 +0330

    Create DTOs User DTOs

---
+++ Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/DTOs/User/CreateUserDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureExample.Application.DTOs.User
+{
+    public class CreateUserDto
+    {
+        
+    }
+}
