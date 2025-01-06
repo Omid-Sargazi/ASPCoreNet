6e287450e7c23a1c44ecd125c3ae61c763aa4480
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-06 10:48:51 +0330

    A query to retrieve a task by its ID

--- Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/Queries/GetTaskById/GetTaskByIdQuery.cs
+++ Project07/CleanArchitectureExample/src/Application/CleanArchitectureExample.Application/Queries/GetTaskById/GetTaskByIdQuery.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArchitectureExample.Application.DTOs;
+using MediatR;
 
 namespace CleanArchitectureExample.Application.Queries.GetTaskById
 {
-    public class GetTaskByIdQuery
+    public class GetTaskByIdQuery:IRequest<TaskItemDto>
     {
-        
+        public Guid TaskId {get;set;}
     }
 }
