08b2793a6548de369a1b80b96b509c2f2f189956
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-10 00:29:44 +0330

    define models

---
+++ Project07/EnhanceCodes/Models/ProductUpdateDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnhanceCodes.Models
+{
+    public class ProductUpdateDto
+    {
+        [Required(ErrorMessage ="Product name is required.")]
+        [StringLength(100,MinimumLength =2,ErrorMessage ="Product name must be between 2 and 100 characters.")]
+        public string Name {get;set;}
+
+        [Required(ErrorMessage ="Price is required.")]
+         [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
+        public decimal Price {get;set;}
+    }
+}
