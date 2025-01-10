fc915dbfe8fbba4747acea7a564a7ec2ef6d507b
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-10 23:40:58 +0330

    define enums

---
+++ Project08/Mainbonsazebackend/BonSaze.Domains.Domain/Enums/CreditRatingIssueReferenceType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonSaze.Domains.Domain.Enums
+{
+    public enum CreditRatingIssueReferenceType
+    {
+        Internal = 1,
+         External = 2
+    }
+}
---
+++ Project08/Mainbonsazebackend/BonSaze.Domains.Domain/Enums/public enum Rating.cs
@@ -0,0 +1,10 @@
+namespace BonSaze.Domains.Domain.Enums
+{
+    public enum Rating
+    {
+        Excellent = 1,
+        Good = 2,
+        Average = 3,
+        Poor = 4
+    }
+}
