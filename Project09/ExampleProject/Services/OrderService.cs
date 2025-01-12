2c8ba46bf756394a411cfac58fb503f5a1ff8786
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-12 21:01:11 +0330

    Handles the logic for placing orders

--- Project09/ExampleProject/Services/OrderService.cs
+++ Project09/ExampleProject/Services/OrderService.cs
@@ -2,11 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExampleProject.Entities;
+using ExampleProject.Repositories;
 
 namespace ExampleProject.Services
 {
     public class OrderService
     {
-        
+        private readonly IOrderRepository _orderRepository;
+        private readonly ICustomerRepository _customerRepository;
+
+        public OrderService(IOrderRepository orderRepository, ICustomerRepository customerRepository)
+        {
+            _orderRepository = orderRepository;
+            _customerRepository = customerRepository;
+        }
+
+        public void PlaceOrder(Guid customerId, List<OrderItem> items)
+        {
+            var customer = _customerRepository.GetById(customerId);
+            if (customer == null)
+                throw new Exception("Customer not found.");
+
+            var order = new Order(customerId);
+            items.ForEach(order.AddItem);
+
+            _orderRepository.Save(order);
+        }
+
+        public Order GetOrder(Guid orderId)
+        {
+            return _orderRepository.GetById(orderId);
+        }
     }
 }
