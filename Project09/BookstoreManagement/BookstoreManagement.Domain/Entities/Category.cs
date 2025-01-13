cea7748f3993a1534740d22bf20eb9998dcd3937
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-13 22:07:05 +0330

    Category Entity

--- Project09/BookstoreManagement/BookstoreManagement.Domain/Entities/Category.cs
+++ Project09/BookstoreManagement/BookstoreManagement.Domain/Entities/Category.cs
@@ -7,6 +7,16 @@ namespace BookstoreManagement.Domain.Entities
 {
     public class Category
     {
-        
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public List<Book> Books { get; private set; } = new List<Book>();
+
+         public Category(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Category name cannot be empty.");
+            Name = name;
+        }
     }
 }
