fe58a786224585e53d897ddf1bd020338cdcb392
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-14 00:45:25 +0330

    create a simple console app to test the connection and query

---
+++ Project09/TestSQLServerConnection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+class Program
+{
+     static void Main(string[] args)
+    {
+        string connectionString = "Server= localhost; Database=BookStore; User Id=sa; password=15935755Omid$@;Encrypt=False;";
+
+        using(var connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection successful!");
+
+                string query = "SELECT name FROM sys.databases;";
+                using(var command = new SqlCommand(query,connection))
+                    using(var reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                        }
+                    }
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine($"Connection failed: {ex.Message}");
+            }
+        }    
+    }
+}
