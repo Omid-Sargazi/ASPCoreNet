04cf719cd1b27a3cb4056590fc47c4dc61536a57
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-13 22:05:40 +0330

    Inventory Entity

--- Project09/BookstoreManagement/BookstoreManagement.Domain/Entities/Inventory.cs
+++ Project09/BookstoreManagement/BookstoreManagement.Domain/Entities/Inventory.cs
@@ -7,6 +7,39 @@ namespace BookstoreManagement.Domain.Entities
 {
     public class Inventory
     {
-        
+        public int Id { get; private set; }
+        public int BookId { get; private set; }
+        public Book Book { get; private set; }
+        public int Quantity { get; private set; }
+
+
+
+        public Inventory(int bookId, int quantity)
+        {
+            if (quantity < 0)
+                throw new ArgumentException("Quantity cannot be negative.");
+
+            BookId = bookId;
+            Quantity = quantity;
+        }
+
+         public void AddStock(int amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Stock amount must be positive.");
+
+            Quantity += amount;
+        }
+
+
+         public void RemoveStock(int amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Stock amount must be positive.");
+            if (amount > Quantity)
+                throw new InvalidOperationException("Cannot remove more stock than available.");
+
+            Quantity -= amount;
+        }
     }
 }
