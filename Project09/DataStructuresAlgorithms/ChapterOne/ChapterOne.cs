bc502ec57a729d2227317fcc77a2ae4feb7720ca
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-16 13:13:28 +0330

    Selection Sort

--- Project09/DataStructuresAlgorithms/ChapterOne/ChapterOne.cs
+++ Project09/DataStructuresAlgorithms/ChapterOne/ChapterOne.cs
@@ -15,10 +15,41 @@ namespace DataStructuresAlgorithms.ChapterOne
             string[] months = new string[12];
             for(int i=1; i<=12;i++)
             {
-                months[i-1] = new DateTime(2023,i,1).ToString();
+                months[i-1] = new DateTime(2023,i,1).ToString("MMMM");
             }
 
             return months;
        }
+
+
+       public int[,] MultiplicationTable()
+       {
+        int [,] table = new int[10, 10];
+        for(int i=0;i<10;i++)
+        {
+            for(int j=0;j<10;j++)
+                table[i,j] = (i+1)*(j+1);
+        }
+
+        return table; 
+
+       }
+       
+
+       public int[] SelectionSort(int[] arr)
+       {
+        for(int i=0;i<arr.Length;i++)
+        {
+            int minIndex = i;
+            for (int j=i+1; j<arr.Length;j++)
+                if(arr[j]<arr[minIndex])
+                    minIndex=j;
+            (arr[i], arr[minIndex])= (arr[minIndex], arr[i]);
+        }
+        return arr;
+       }
+
     }
+
+
 }
--- Project09/DataStructuresAlgorithms/Program.cs
+++ Project09/DataStructuresAlgorithms/Program.cs
@@ -17,3 +17,30 @@ foreach(var month in months)
 {
     Console.WriteLine(month);
 }
+
+//Multi-Dimensional Arrays
+//Declaration
+int[,] grid = {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+
+Console.WriteLine(grid[0,0]);
+
+
+//Multiplication table
+
+var table= C1.MultiplicationTable();
+
+for(int i=0; i<10; i++)
+    for(int j=0; j<10; j++)
+        Console.Write($"{table[i, j]+"/n"+"**"}");
+    Console.WriteLine();
+
+int[] arr=[10,8,199,123,100,-10,4740,];
+var sorted = C1.SelectionSort(arr);
+for(int i=0;i<arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
