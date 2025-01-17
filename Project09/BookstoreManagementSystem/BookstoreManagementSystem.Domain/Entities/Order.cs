5d35a99bf2743e85d2ee951684d664765ff55964
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-17 12:35:50 +0330

    create Order entity

--- Project09/BookstoreManagementSystem/BookstoreManagementSystem.Domain/Entities/Order.cs
+++ Project09/BookstoreManagementSystem/BookstoreManagementSystem.Domain/Entities/Order.cs
@@ -2,11 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookstoreManagementSystem.Domain.Interfaces;
 
 namespace BookstoreManagementSystem.Domain.Entities
 {
-    public class Order
+    public class Order:IAggregateRoot
     {
-        
+        public int Id {get; private set;}
+        public int CustomerId {get; private set;}
+        private Customer Customer {get;  set;}
+        public List<Book> Books {get; set;} = new List<Book>();
+        public decimal TotalAmount => Books.Sum(b=>b.Price.Amount);
+
+        public Order(int customerId)
+        {
+            if(customerId <= 0)
+                throw new ArgumentException("Invalid customer ID.");
+            
+            CustomerId = customerId;
+        }
+
+        public void AddBook(Book book)
+        {
+            if(book == null)
+                throw new ArgumentNullException(nameof(book));
+            Books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if(book == null)
+                throw new ArgumentNullException(nameof(book));
+            Books.Add(book);
+
+        }
+
     }
 }
