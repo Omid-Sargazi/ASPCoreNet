f0006b54bac940cf696ea6de86a40f6fa27162dd
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-18 22:57:11 +0330

    define a simple delegate

---
+++ Project10/DelegateExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace DelegateExamples
+{
+    public class Program
+    {
+        public delegate void MyDelegate(string msg);
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate");
+            MyDelegate del = new MyDelegate(DisplayMessage);
+            del("omid");
+            
+        }
+
+        public static void DisplayMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+
+    }
+}
