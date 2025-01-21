787519787bb10c5ba37befdcb0895816857ad232
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-21 14:30:04 +0330

    define Order entity

--- Project10/Tests/Test02/MyApp/src/MyApp.Domain/Entities/Order.cs
+++ Project10/Tests/Test02/MyApp/src/MyApp.Domain/Entities/Order.cs
@@ -7,6 +7,31 @@ namespace MyApp.Domain.Entities
 {
     public class Order
     {
-        
+        public Guid Id {get; set;}
+        public DateTime OrderDate {get; private set;}
+        public List<Product> Products{get; set;} = new List<Product>();
+
+        public Order()
+        {
+            Id = Guid.NewGuid();
+            OrderDate = DateTime.UtcNow;
+        }
+
+
+        public void AddProduct(Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+
+            Products.Add(product);
+        }
+
+
+        public decimal GetTotalAmount()
+        {
+            return Products.Sum(p => p.Price);
+        }
+
+
     }
 }
