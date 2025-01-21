cad4ef2006b2a3ed7e1c90bc46966258272ff31c
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-21 19:22:26 +0330

    Generic Interface

--- Project10/Tests/Test02/MyApp/src/MyApp.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
+++ Project10/Tests/Test02/MyApp/src/MyApp.Domain/obj/Debug/net9.0/MyApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787519787bb10c5ba37befdcb0895816857ad232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2f35a89b009332afe1378c310369a60c868611")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
