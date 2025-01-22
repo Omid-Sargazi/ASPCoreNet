88e6d549ef6df9260189e3d276340881e04fa733
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-22 22:59:38 +0330

    Simple Delegate and Event

---
+++ Project11/EventsAndDelegates/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public delegate void Notify(string message);
+    public static void Main(string[] args)
+    {
+        Notify notification = ShowMessage;
+        Console.WriteLine("Hello World!");
+        notification("Hello, this is a simple delegate example!");
+    }
+
+    static void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
