32f5edf5b79576ec3261f7951c1bc7db99314b97
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-25 22:35:08 +0330

    Implement Concrete Builders HawaiianPizzaBuilder

--- Project12/BuilderPattern/obj/Debug/net9.0/BuilderPattern.AssemblyInfo.cs
+++ Project12/BuilderPattern/obj/Debug/net9.0/BuilderPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuilderPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a6f40be232e925fc15d9d233ad70dccb2a6c68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a93b632c4c06e1d5f1e926733858487afe4f688")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuilderPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuilderPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
