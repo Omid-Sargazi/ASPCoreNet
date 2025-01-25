ffd2a9a31e91901fd5bf14c8f240a4aa8f52badb
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-25 21:01:04 +0330

    create interface for furniture factory

---
+++ Project12/FactoryPattern/FernitureAbstarctFactory/Interfaces/IChair.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.FernitureAbstarctFactory.Interfaces
+{
+    public interface IChair
+    {
+        public void Create();
+    }
+}
