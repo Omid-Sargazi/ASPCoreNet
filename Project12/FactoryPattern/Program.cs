58b5922965de39d530cbc1b80993b64514027b06
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-25 18:27:24 +0330

    usage of them

---
+++ Project12/FactoryPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using FactoryPattern.Notification.Factory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Factory Pattern");
+
+        NotificationFactory notification = new CreateEmailNotification();
+        notification.Notify("This is an email notification");
+        notification = new CreateSMSNotification();
+        notification.Notify("This is an SMS notification");
+    }
+}
