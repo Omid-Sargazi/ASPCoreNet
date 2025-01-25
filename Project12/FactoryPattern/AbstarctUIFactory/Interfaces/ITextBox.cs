ab43c757e81d39c8b1560de8bd872f7ec787f1ba
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-25 21:08:46 +0330

    Define abstract product interfaces for buttons, text boxes, and checkboxes

---
+++ Project12/FactoryPattern/AbstarctUIFactory/Interfaces/ITextBox.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoryPattern.AbstarctUIFactory.Interfaces
+{
+    public interface ITextBox
+    {
+        public void CreateTextBox();
+    }
+}
