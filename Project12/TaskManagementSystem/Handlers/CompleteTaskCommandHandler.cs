7dda59299519b95ea6432e4576efde4bdee192d8
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-26 21:37:11 +0330

    complete other handlers

--- Project12/TaskManagementSystem/Handlers/CompleteTaskCommandHandler.cs
+++ Project12/TaskManagementSystem/Handlers/CompleteTaskCommandHandler.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskManagementSystem.Commands;
+using TaskManagementSystem.Models;
 using TaskManagementSystem.Responses;
 
 namespace TaskManagementSystem.Handlers
 {
     public class CompleteTaskCommandHandler : CommandHandler<CompleteTaskCommand, CommandResponse>
     {
+        private static readonly Dictionary<string, Taskes> Taskes = new Dictionary<string, Taskes>();
         public override Task<CommandResponse> Execute(CompleteTaskCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if(cancellationToken.IsCancellationRequested)
+                return Task.FromResult(CommandResponseCanceled.CreateCanceled(command.TaskId,command.TaskName,"Task completion canceled."));
+
+            if(Taskes.TryGetValue(command.TaskId, out var taskes))
+            {
+                taskes.IsCompleted = true;
+                return Task.FromResult(CommandResponseSuccessful.CreateSuccessful(command.TaskId, command.TaskName, "Task completed successfully."));
+            }
+
+            return Task.FromResult(CommandResponseFailed.CreateFailed(command.TaskId, "Unknown Task", "Task not found."));
         }
     }
 }
--- Project12/TaskManagementSystem/Handlers/UpdateTaskCommandHandler.cs
+++ Project12/TaskManagementSystem/Handlers/UpdateTaskCommandHandler.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskManagementSystem.Commands;
+using TaskManagementSystem.Models;
 using TaskManagementSystem.Responses;
 
 namespace TaskManagementSystem.Handlers
 {
     public class UpdateTaskCommandHandler : CommandHandler<UpdateTaskCommand, CommandResponse>
     {
+        private static readonly Dictionary<string, Taskes> Taskes = new Dictionary<string, Taskes>();
         public override Task<CommandResponse> Execute(UpdateTaskCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if(cancellationToken.IsCancellationRequested)
+                return Task.FromResult(CommandResponseCanceled.CreateCanceled(command.TaskId,command.TaskName));
+            
+            if(Taskes.TryGetValue(command.TaskId, out var taskes))
+            {
+                taskes.TaskName = command.TaskName;
+                taskes.Description = command.NewDescription;
+                return Task.FromResult(CommandResponseSuccessful.CreateSuccessful(command.TaskId, command.TaskName));
+            }
+
+            return Task.FromResult(CommandResponseFailed.CreateFailed(command.TaskId,command.TaskName,"Task not found."));
         }
     }
 }
