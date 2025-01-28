d358304f7aa5e58a970f0bf528b2dcbabaeb32a3
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-28 20:03:40 +0330

    Querying a List of Custom Objects

---
+++ Project13/QueryingObjects/Models/Person.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QueryingObjects.Models
+{
+    public class Person
+    {
+        public string Name {get; set;}
+        public int Age {get; set;}
+        public string City {get; set;}
+        
+    }
+}
---
+++ Project13/QueryingObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using QueryingObjects.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+
+        List<Person> people = new List<Person> {
+            new Person {Name = "Alice", Age=25, City="New Yourk"},
+            new Person {Name = "Bob", Age=30, City="LA"},
+            new Person {Name = "Charlie", Age=35, City="LA"},
+            new Person {Name = "David", Age=40, City="Chicago"},
+            new Person {Name = "Eve", Age=28, City="LA"}
+        };
+
+
+        var newYorkers = from person in people
+                            where person.City == "New Yourk"
+                            select person;
+        Console.WriteLine("People from NewYork:");
+        foreach(var person in newYorkers)
+        {
+            Console.WriteLine($"{person.Name}, {person.Age}");
+        }
+
+    }
+}
