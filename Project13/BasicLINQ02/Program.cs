caa0d621d39dafd42fef0db413377874de5a924e
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-29 20:45:29 +0330

    Basic LINQ Query on a List of Integers

---
+++ Project13/BasicLINQ02/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5,6,7,8,9,10 };
+
+        var evenNumbers = from num in numbers
+                            where num%2==0
+                            select num;
+        
+        foreach(var num in evenNumbers)
+            Console.WriteLine("Even Numbers :"+num);
+
+    }
+}
