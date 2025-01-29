196508439a7902bdda017e02af683d8b5da17546
Omid-Sargazi <om.sargazi@gmail.com>
2025-01-29 20:52:49 +0330

    simple LINQ

--- Project13/BasicLINQ02/Program.cs
+++ Project13/BasicLINQ02/Program.cs
@@ -12,5 +12,28 @@
         foreach(var num in evenNumbers)
             Console.WriteLine("Even Numbers :"+num);
 
+        var greaterThanFive = numbers.Where(num => num >=5);
+        Console.WriteLine("gretaer than five:");
+        foreach(var num in greaterThanFive)
+            Console.WriteLine(num);
+
+        //Sorting with OrderBy
+        var sortedNumbers = numbers.OrderBy(num => num);
+        Console.WriteLine("Sorted numbers:");
+        foreach(var num in sortedNumbers)
+            Console.WriteLine(num);
+
+        //Sorting with OrderByDescending
+        var sortedDescending = numbers.OrderByDescending(num => num);
+        Console.WriteLine("Odrer by desending.");
+        foreach(var num in sortedDescending)
+            Console.WriteLine(num);
+
+        //Selecting Specific Properties
+        var squares = numbers.Select(num => num*num);
+        Console.WriteLine("Squares of numbers:");
+        foreach(var num in squares)
+            Console.WriteLine(num);
+
     }
 }
