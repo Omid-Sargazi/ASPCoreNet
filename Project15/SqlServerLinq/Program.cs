d39ade29004f471a2004156edb4f6bb32509f5b3
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-02 21:40:35 +0330

    Advanced query

--- Project15/SqlServerLinq/Program.cs
+++ Project15/SqlServerLinq/Program.cs
@@ -33,6 +33,15 @@ public class Program
             {
                 Console.WriteLine($"Young Student: {student.Name}, Age:{student.Age}");
             }
+
+            //Advanced Queries
+            var studentCourses = context.Enrollments
+            .Join(context.Students, e=>e.StudentId,s=>s.StudentId,(e,s)=>new {e,s})
+            .Join(context.Courses,es=>es.e.CourseId,c=>c.CourseId,(es,c)=> new{
+                studentName = es.s.Name,
+                courseTitle = c.Title,
+            }).ToList();
+
         }
     }
 
