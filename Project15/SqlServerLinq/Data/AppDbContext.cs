62a4b20da7984c06cc2796fc3914ab12f88413bf
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-02 21:15:51 +0330

    create confoguration

---
+++ Project15/SqlServerLinq/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SqlServerLinq.Models;
+
+namespace SqlServerLinq.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Student> Students {get; set;}
+        public DbSet<Enrollment> Enrollments {get; set;}
+        public DbSet<Course> Courses {get; set;}
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasKey(s => s.StudentId);
+            modelBuilder.Entity<Course>().HasKey(c => c.CourseId);
+            modelBuilder.Entity<Enrollment>().HasKey(e => e.EnrollmentId);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.StudentId);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Course)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(e => e.CourseId);
+        }
+
+    }
+}
