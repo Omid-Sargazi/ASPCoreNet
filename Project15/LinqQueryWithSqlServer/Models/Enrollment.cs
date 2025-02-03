cf2d8c0d16c969c9f8759a4e6cfdd34b79c0b725
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-03 10:59:18 +0330

    define all models

---
+++ Project15/LinqQueryWithSqlServer/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqQueryWithSqlServer.Models
+{
+    public class Enrollment
+    {
+       public int EnrollmentId {get; set;}
+       public int StudentId {get; set;}
+       public int CourseId {get; set;}
+
+       public Student Student {get; set;}
+       public Course Course {get; set;} 
+    }
+}
---
+++ Project15/LinqQueryWithSqlServer/Models/Student.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqQueryWithSqlServer.Models
+{
+    public class Student
+    {
+       public int StudentId {get; set;}
+       public string Name {get; set;} 
+       public int Age {get; set;}
+       public List<Enrollment> Enrollments {get; set;}
+    }
+}
