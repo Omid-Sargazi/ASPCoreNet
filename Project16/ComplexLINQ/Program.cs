12afdc0f1d6d8ac602b676c2a88517d997e66632
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-04 23:33:45 +0330

    select many

---
+++ Project16/ComplexLINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello LINQ");
+
+        var listOfLists = new List<List<int>> 
+        {
+            new List<int>{1,2,3},
+            new List<int> {4,5},
+            new List<int> {6}
+        };
+        var result = listOfLists.SelectMany(list => list).ToList();
+        foreach(var i in result)
+        {
+            Console.WriteLine(i);
+        }
+
+        var listOfArray = new List<int[]> 
+        {
+            new int[] {1,2,3},
+            new int[] {4, 5, 6},
+            new int[] {7, 8}
+        };
+        result = listOfArray.SelectMany(array => array).ToList();
+
+        var words = new List<string> {"hello","world"};
+        var wordsResults = words.SelectMany(word => word.ToCharArray()).ToList();
+
+    }
+}
