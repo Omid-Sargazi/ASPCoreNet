282b5fd60f2b614a482d78862c9d757b69dab313
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-07 12:32:00 +0330

    usage and implement factory

--- Project15/AbstarctFactoryPattern/Program.cs
+++ Project15/AbstarctFactoryPattern/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using AbstarctFactoryPattern;
+using AbstarctFactoryPattern.Factory;
+using AbstarctFactoryPattern.interfaces;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+
+        Console.WriteLine("Eneter config for factory");
+        string config = Console.ReadLine();
+
+        IUIFactory factory;
+
+        if(config == "win")
+        {
+            factory = new WinFactory();
+        }
+        else if(config == "mac")
+        {
+            factory = new MacOSFactory();
+        }
+        else{
+            throw new ArgumentException("Enetr a correct config");
+        }
+
+        ClientUI client = new ClientUI(factory);
+        client.RenderUI();
+    }
+}
