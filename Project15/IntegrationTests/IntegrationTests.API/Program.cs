aac421861c890cb491fb21897442d79c05985c35
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-09 20:37:57 +0330

    Configure the services in Program.cs

---
+++ Project15/IntegrationTests/IntegrationTests.API/Program.cs
@@ -0,0 +1,62 @@
+using IntegrationTests.API.Data;
+using IntegrationTests.API.Implements;
+using IntegrationTests.API.Interfaces;
+using IntegrationTests.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<AppDbContext>(options=>
+            options.UseInMemoryDatabase("Testdb")
+        );
+
+        builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+        builder.Services.AddScoped<IPaymentGateway, PaymentGateway>();
+        builder.Services.AddScoped<OrderService>();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+
+    }
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+}
