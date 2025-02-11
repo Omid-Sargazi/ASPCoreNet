49e90fe91d7a8af362bfe766ac6918aa911113f8
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-11 22:32:18 +0330

    define models

---
+++ Project15/Authentication/Authentication.API/Models/ApplicantUser.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.API.Models
+{
+    public class ApplicantUser
+    {
+         public bool IsSuperAdmin { get; set; }
+        public string? BusinessCode { get; set; }
+        public string? NationalCode { get; set; }
+        public string? CompanyNationalCode { get; set; }
+        public string? ApplicantUnitName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? CompanyRole { get; set; }
+        public string? MobileNumber { get; set; }
+        public bool IsAgent { get; set; }
+        public string? AgentNationalCode { get; set; }
+        public bool IsDeleted { get; protected set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public void SetDelete()
+        {
+            IsDeleted = true;
+        }
+    }
+}
