dd4c96c377022304d2d0a91b4196ce4c6ccb72c7
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-12 12:38:12 +0330

    Configure InMemory Database

---
+++ Project17/Authentication/Authentication.API/Program.cs
@@ -0,0 +1,38 @@
+using System.Text;
+using Authentication.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options=>
+            options.UseInMemoryDatabase("AuthTst")
+        );
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options=>{
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperSecretKey1234567890"))
+            };
+        });
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
+    }
+}
