9884649fbc475d22524565c5409895f46adf5af1
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-14 21:27:39 +0330

    define modesla and seed data

---
+++ Project17/LinqExample02/Linq/Models/Employee.cs
@@ -0,0 +1,8 @@
+public class Employee
+{
+        public int Id { get; set; }
+            public string Name { get; set; }
+            public string Role { get; set; }
+            public decimal Salary { get; set; }
+            public int DepartmentId { get; set; }
+}
