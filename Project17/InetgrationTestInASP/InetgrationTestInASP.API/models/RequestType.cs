aa6ab7042a0ff6069f24466197d8866aba3e3c80
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-16 21:36:21 +0330

    RequestType is an enum that represents different types of requests a user can make.

---
+++ Project17/InetgrationTestInASP/InetgrationTestInASP.API/models/RequestType.cs
@@ -0,0 +1,11 @@
+namespace InetgrationTestInASP.API.models
+{
+    public enum RequestType
+    {
+        GeneralInquiry = 1,
+        TechnicalSupport = 2,
+        BillingIssue = 3,
+        AccountUpgrade = 4,
+        QuickIndustrialUnitAssessment = 5
+    }
+}
