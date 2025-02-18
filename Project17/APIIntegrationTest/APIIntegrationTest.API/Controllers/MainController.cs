5dae8c9c012de994a6519c2380ec1fbb9a1f9cc0
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-18 21:41:30 +0330

    Implement the Main Controller

--- Project17/APIIntegrationTest/APIIntegrationTest.API/Controllers/MainController.cs
+++ Project17/APIIntegrationTest/APIIntegrationTest.API/Controllers/MainController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using APIIntegrationTest.API.Contracts.Commands;
 using APIIntegrationTest.API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,5 +19,27 @@ namespace APIIntegrationTest.API.Controllers
         {
             _distributor = distributor;
         }
+
+        protected IActionResult OkApiResult(dynamic tResult)
+        {
+            return Ok(new ApiResult(CommandResponseSuccessful.CreateSuccessful(), tResult));
+        }
+
+        protected IActionResult OkApiResult()
+        {
+            return Ok(new ApiResult(CommandResponseSuccessful.CreateSuccessful()));
+        }
+
+        protected IActionResult OkApiCommandResponseResult(CommandResponse tResult)
+        {
+            return Ok(new ApiResult(tResult));
+        }
+
+        protected IActionResult OkApiFileResult(byte[] bytes, string contentType)
+        {
+            return File(bytes, contentType);
+        }
+
+
     }
 }
