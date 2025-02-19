b0c6de8ac88cdd4f27e18eaed6f1b4c3c558fbda
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-20 00:07:49 +0330

    define database for new entity

---
+++ Project18/CQRSPattern01/ECommerce/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class AppDbContext:IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
