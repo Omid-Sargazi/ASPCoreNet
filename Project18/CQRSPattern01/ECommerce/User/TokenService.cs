15b870d4f9abc391dc7749f44cd7e4aa867e0d0d
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-20 00:40:23 +0330

    Token Service (TokenService.cs)

--- Project18/CQRSPattern01/ECommerce/User/TokenService.cs
+++ Project18/CQRSPattern01/ECommerce/User/TokenService.cs
@@ -1,12 +1,46 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using ECommerce.Entities;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ECommerce.User
 {
     public class TokenService
     {
-        
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config)
+        {   
+            _config = config;
+        }
+
+        public string GenerateToken(ApplicationUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("FullName", user.FullName),
+            };
+            
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires:DateTime.UtcNow.AddHours(1),
+                signingCredentials:credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
