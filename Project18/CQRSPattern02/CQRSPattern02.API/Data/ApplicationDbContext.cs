5e71e4e59bd4cc2ebd5cfc7c517b5c9dfd7c92d3
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-20 15:47:08 +0330

    define database

---
+++ Project18/CQRSPattern02/CQRSPattern02.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSPattern02.API.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
