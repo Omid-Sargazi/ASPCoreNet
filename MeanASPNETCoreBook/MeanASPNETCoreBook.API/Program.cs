497e3a771a852c68145103976e4e34690ff8f6be
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-22 18:48:59 +0330

    define exceptions and static files

--- MeanASPNETCoreBook/MeanASPNETCoreBook.API/Program.cs
+++ MeanASPNETCoreBook/MeanASPNETCoreBook.API/Program.cs
@@ -2,20 +2,34 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        Console.WriteLine("hello");
+       var builder = WebApplication.CreateBuilder();
 
-        var app = builder.Build();
+       var app = builder.Build();
 
-        app.Use(async (context, next)=>{
-            Console.WriteLine($"Incoming request: {context.Request.Method} {context.Request.Path}");
-            await next();
-        });
+       app.UseDeveloperExceptionPage();
+       app.UseStaticFiles();
 
-        app.MapGet("/",()=> "Hello World");
-        // app.MapGet("/about",()=>"this is about page");
+       app.Use(async (context, next)=>{
+        Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+        await next();
+       });
 
-        // app.MapGet("/user",()=> new{Name = "Omid", Age=42});
+       app.Use(async (context, next)=>{
+        await next();
+        if(!context.Response.HasStarted)
+            await context.Response.WriteAsync("Middleware says: Welcome!");
+        
+        // await next();
+       });
 
-        app.Run();
+       app.MapGet("/",()=>"Hello World");
+
+       app.MapGet("/about",()=>"this is about page");
+
+       app.MapGet("/user",() => new {Name = "Omid", Age=42});
+
+       app.Run();
+
     }
 }
