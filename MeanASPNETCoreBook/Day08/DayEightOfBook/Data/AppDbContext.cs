069f402fc3568c64a351edf5b00718713f4e68d5
Omid-Sargazi <om.sargazi@gmail.com>
2025-02-24 23:23:31 +0330

    defrine database and configure it

--- MeanASPNETCoreBook/Day08/DayEightOfBook/Data/AppDbContext.cs
+++ MeanASPNETCoreBook/Day08/DayEightOfBook/Data/AppDbContext.cs
@@ -10,10 +10,9 @@ namespace DayEightOfBook.Data
 {
     public class AppDbContext:IdentityDbContext<ApplicationUser>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
-        {
-            
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+
+        public DbSet<Product> Products {get; set;}
     }
 
  
