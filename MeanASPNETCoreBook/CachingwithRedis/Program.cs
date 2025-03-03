5016074805cf8717df192324af65089112eb211b
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-03 12:25:37 +0330

    request to cached endpoint

--- MeanASPNETCoreBook/CachingwithRedis/Program.cs
+++ MeanASPNETCoreBook/CachingwithRedis/Program.cs
@@ -1,38 +1,60 @@
+using CachingwithRedis.Data;
+using CachingwithRedis.Model;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Swashbuckle.AspNetCore.Swagger;
 public class Program
 {
-    public static void Main(string[] args)
+public static void Main(string[] args)
     {
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add Redis distributed caching
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetConnectionString("Redis");
-    options.InstanceName = "RedisCachingExample_";
-});
+    builder.Services.AddDbContext<AppDbContext>(options=> options.UseSqlite("Data Source=app.db"));
 
-// Add in-memory caching (optional)
-builder.Services.AddMemoryCache();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+    // Add Redis distributed caching
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = builder.Configuration.GetConnectionString("Redis");
+        options.InstanceName = "RedisCachingExample_";
+    });
 
-var app = builder.Build();
+    // Add in-memory caching (optional)
+    builder.Services.AddMemoryCache();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    // app.UseSwaggerUI();
-}
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+    var app = builder.Build();
 
-app.Run();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    using(var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.EnsureCreated();
+
+        if(!db.Products.Any())
+        {
+            db.Products.AddRange(
+                new Product {Name = "Laptop", Price=999.99m, Description = "High Performance"},
+                new Product {Name = "Phone", Price=599.99m, Description = "Last smartphone"},
+                new Product {Name = "Tablet", Price=399.99m, Description = "portable tablet"}
+            );
+            db.SaveChanges();
+        }
+    }
+
+    app.Run();
     }
 }
