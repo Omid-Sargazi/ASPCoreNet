d48bde9447a802ff9b8ce5ffae64a3736c108fce
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-08 23:12:33 +0330

    Configure JWT Authentication using Keycloak

--- Project21/LoggigMonitoringMatter/Program.cs
+++ Project21/LoggigMonitoringMatter/Program.cs
@@ -1,3 +1,6 @@
+using LoggigMonitoringMatter.Day5Practice;
+using LoggigMonitoringMatter.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Serilog;
 
 public class Program
@@ -6,7 +9,28 @@ public class Program
     {
         Console.WriteLine("Hello");
 
+        var productCatalog = new Catalog<Product>();
+
+        productCatalog.AddItem(new Product{Name = "soda",Price = 125.3m,Discount = 12.2m});
+        productCatalog.AddItem(new Product{Name = "soda",Price = 125.3m,Discount = null});
+        productCatalog.AddItem(new Product{Name = "soda",Price = 125.3m,Discount = 12.2m});
+
+        var discountedItems = productCatalog.FilterBy(p => p.Discount.HasValue);
+        Console.WriteLine("Discounted Products");
+        foreach(var p in discountedItems)
+        {
+            Console.WriteLine($"{p.Name} - Price: {p.Price}, Discount: {p.Discount:P0}");
+        }
+
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options=>{
+            options.Authority = "http://localhost:8080/realms/MyApp";
+            options.Audience = "myapp-client";
+            options.RequireHttpsMetadata = false;
+        });
+
         Log.Logger = new LoggerConfiguration()
         .MinimumLevel.Information()
         .WriteTo.Console()
