420263a985f1ccfc0bde04e01916028f79f7dde4
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-08 23:27:16 +0330

    Configure OAuth in Program.cs

---
+++ Project21/securingAPIswithOAuth2OpenID/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "http://localhost:8080/realms/MyApp";
+        options.Audience = "myapp-client";
+        options.RequireHttpsMetadata = false; // Dev only
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+app.Run();
+
