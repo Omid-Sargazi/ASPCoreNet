6a0a9ac9b88c4c66b2126d6f55e13c9f9dce1a07
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-13 20:00:58 +0330

    define,create and implementaions

--- Project23/OrderProcessingSystem/Implements/OrderRepository.cs
+++ Project23/OrderProcessingSystem/Implements/OrderRepository.cs
@@ -2,11 +2,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingSystem.Data;
+using OrderProcessingSystem.Interfacses;
+using OrderProcessingSystem.Models;
 
 namespace OrderProcessingSystem.Implements
 {
-    public class OrderRepository
+    public class OrderRepository : IOrderRepository
     {
-        
+        private readonly ECommerceDbContext _dbContext;
+
+        public OrderRepository(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<string> CreateAsync(Order order)
+        {
+            order.Id = Guid.NewGuid().ToString();
+            order.OrderDate = DateTime.UtcNow;
+            order.Status = OrderStatus.Created;
+
+            await _dbContext.Orders.AddAsync(order);
+            await _dbContext.SaveChangesAsync();
+            return order.Id;
+        }
+
+        public async Task<List<Order>> GetByCustomerIdAsync(int customerId)
+        {
+            return await _dbContext.Orders
+            .Include(o => o.Items)
+            .Where(o => o.CustomerId == customerId)
+            .ToListAsync();
+        }
+
+        public async Task<Order> GetByIdAsync(string id)
+        {
+           return await _dbContext.Orders
+           .Include(o => o.Items)
+           .FirstOrDefaultAsync(o => o.Id==id);
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            _dbContext.Orders.Update(order);
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
--- Project23/OrderProcessingSystem/Implements/ProductRepository.cs
+++ Project23/OrderProcessingSystem/Implements/ProductRepository.cs
@@ -2,11 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingSystem.Data;
+using OrderProcessingSystem.Interfacses;
+using OrderProcessingSystem.Models;
 
 namespace OrderProcessingSystem.Implements
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
-        
+        private readonly ECommerceDbContext _dbContext;
+        public ProductRepository(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;   
+        }
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _dbContext.Products.FindAsync(id);
+        }
+
+        public async Task<List<Product>> GetByIdsAsync(List<int> ids)
+        {
+            return await _dbContext.Products.Where(p => ids.Contains(p.Id)).ToListAsync();
+        }
+
+        public async Task UpdateStockAsync(int productId, int quantity)
+        {
+            var product = await _dbContext.Products.FindAsync(productId);
+            if(product !=null)
+            {
+                product.StockQuantity -= quantity;
+                await _dbContext.SaveChangesAsync();
+
+            }
+        }
     }
 }
