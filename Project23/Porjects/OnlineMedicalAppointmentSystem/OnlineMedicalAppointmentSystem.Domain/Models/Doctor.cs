f1eadf0302cd5aa50ebb68a5c68ba43310f92b2f
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-16 19:27:16 +0330

    defien doctor entity

--- Project23/Porjects/OnlineMedicalAppointmentSystem/OnlineMedicalAppointmentSystem.Domain/Models/Doctor.cs
+++ Project23/Porjects/OnlineMedicalAppointmentSystem/OnlineMedicalAppointmentSystem.Domain/Models/Doctor.cs
@@ -7,6 +7,12 @@ namespace OnlineMedicalAppointmentSystem.Domain.Models
 {
     public class Doctor
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Specialty { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Appointment> Appointments {get; set;} = new List<Appointment>();
+        public List<Schedule> Schedules {get; set;} = new List<Schedule>();
     }
 }
