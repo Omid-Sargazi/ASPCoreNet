94f597e82023ca2affa2c7e5a0034af4eb2c7775
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-16 19:30:23 +0330

    define other entities

--- Project23/Porjects/OnlineMedicalAppointmentSystem/OnlineMedicalAppointmentSystem.Domain/Models/Payment.cs
+++ Project23/Porjects/OnlineMedicalAppointmentSystem/OnlineMedicalAppointmentSystem.Domain/Models/Payment.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace OnlineMedicalAppointmentSystem.Domain.Models
 {
-    public class Payment
+    public class Payment:BaseEntity
     {
-        
+        public int AppointmentId { get; set; }
+        public Appointment Appointment { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentStatus { get; set; } // Pending, Paid, Failed
+        public DateTime TransactionDate { get; set; }
     }
 }
--- Project23/Porjects/OnlineMedicalAppointmentSystem/OnlineMedicalAppointmentSystem.Domain/Models/Schedule.cs
+++ Project23/Porjects/OnlineMedicalAppointmentSystem/OnlineMedicalAppointmentSystem.Domain/Models/Schedule.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace OnlineMedicalAppointmentSystem.Domain.Models
 {
-    public class Schedule
+    public class Schedule:BaseEntity
     {
-        
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsBooked { get; set; }
     }
 }
