111caf228f7f7fe95d79b84ac00bfe2f9eda8772
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-20 12:54:19 +0330

    Inventory Manager for managing products

--- Project23/Patterns/InventoryManagementSystem/InventoryManager.cs
+++ Project23/Patterns/InventoryManagementSystem/InventoryManager.cs
@@ -13,5 +13,32 @@ namespace Patterns.InventoryManagementSystem
         {
             _products.Add(productOfSystem);
         }
+
+        public void UpdateStock(string productId, int quantity)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == productId);
+            if(product !=null)
+                product.Stock +=quantity;
+        }
+
+        public ProductOfSystem SearchProduct(string keyword)
+        {
+            return _products.FirstOrDefault(p => p.Id == keyword || p.Name == keyword);
+        }
+
+        public void RemoveProduct(string productId)
+        {
+            var product = _products.FirstOrDefault(p => p.Id ==productId);
+            if(product !=null)
+                _products.Remove(product);
+        }
+
+         public void DisplayProducts()
+    {
+        foreach (var product in _products)
+        {
+            Console.WriteLine(product);
+        }
+    }
     }
 }
--- Project23/Patterns/InventoryManagementSystem/ProductOfSystem.cs
+++ Project23/Patterns/InventoryManagementSystem/ProductOfSystem.cs
@@ -7,7 +7,7 @@ namespace Patterns.InventoryManagementSystem
 {
     public class ProductOfSystem:ICloneable
     {
-        public int Id {get; set;}
+        public string Id {get; set;}
         public string Name {get; set;}
         public decimal Price {get; set;}
         public int Stock {get; set;}
