1803b3a2b8727eb3b53a69162b1775b46c260e89
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-23 19:28:56 +0330

    using in end a process

---
+++ Project23/UsingAndDisposable/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello");
+
+        using(StreamWriter writer = new StreamWriter("example.txt"))
+        {
+            writer.WriteLine("Hello World");
+        }
+        Console.WriteLine("The file has been written");
+    }
+}
