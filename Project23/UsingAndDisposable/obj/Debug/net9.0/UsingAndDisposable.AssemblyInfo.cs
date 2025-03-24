c0bc8b1535b3493baf95ed9161be4ddb5886c914
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-24 21:05:17 +0330

    Implementor

--- Project23/UsingAndDisposable/obj/Debug/net9.0/UsingAndDisposable.AssemblyInfo.cs
+++ Project23/UsingAndDisposable/obj/Debug/net9.0/UsingAndDisposable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsingAndDisposable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eec403485cefbc9d9be86fa4543bdfa57c72834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20fbded5be7a4084f10fd96c03e92003d0b4147")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsingAndDisposable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsingAndDisposable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
