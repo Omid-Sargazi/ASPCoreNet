d7f5d674b52c070a45b58972699fb0072505fdb8
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-29 13:21:50 +0330

    controle light by mediator

--- Project23/UsingAndDisposable/MediatorPattern/HomeMediator/Light.cs
+++ Project23/UsingAndDisposable/MediatorPattern/HomeMediator/Light.cs
@@ -7,6 +7,18 @@ namespace UsingAndDisposable.MediatorPattern.HomeMediator
 {
     public class Light
     {
-        
+        private IHomeMediator _mediator;
+        private bool _isOn;
+        public void SetMediator(IHomeMediator mediator) => _mediator = mediator;
+        public void TurnOn()
+        {
+            _isOn = true;
+            Console.WriteLine("Light: لامپ روشن شد.");
+        }
+        public void TurnOff()
+        {
+            _isOn = false;
+            Console.WriteLine("Light: لامپ خاموش شد.");
+        }
     }
 }
