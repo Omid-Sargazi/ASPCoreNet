53403f43290ca54c0d033b91c61318bb9090c1fa
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-29 13:30:30 +0330

    define all codes in the home mediator

--- Project23/UsingAndDisposable/MediatorPattern/HomeMediator/HomeMediator.cs
+++ Project23/UsingAndDisposable/MediatorPattern/HomeMediator/HomeMediator.cs
@@ -7,9 +7,31 @@ namespace UsingAndDisposable.MediatorPattern.HomeMediator
 {
     public class HomeMediator : IHomeMediator
     {
+        private DoorSensor _doorSensor;
+        private Light _light;
+        private Fan _fan;
+        public HomeMediator(DoorSensor doorSensor, Light light, Fan fan)
+        {
+            _doorSensor = doorSensor;
+            _light = light;
+            _fan = fan;
+            _doorSensor.SetMediator(this);
+            _light.SetMediator(this);
+            _fan.SetMediator(this);
+        }
+        
         public void Notify(string sender, string eventName)
         {
-            throw new NotImplementedException();
+            if (sender == "DoorSensor" && eventName == "DoorOpened")
+        {
+            _light.TurnOn(); // لامپ رو روشن کن
+            _fan.TurnOn();   // فن رو روشن کن
+        }
+        else if (sender == "DoorSensor" && eventName == "DoorClosed")
+        {
+            _light.TurnOff(); // لامپ رو خاموش کن
+            _fan.TurnOff();   // فن رو خاموش کن
+        }
         }
     }
 }
