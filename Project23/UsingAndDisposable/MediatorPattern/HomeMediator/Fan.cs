57416ca27fe1703a854878a6e2849ef1d1fdc4a3
Omid-Sargazi <om.sargazi@gmail.com>
2025-03-29 13:26:38 +0330

    control fan by mediator

--- Project23/UsingAndDisposable/MediatorPattern/HomeMediator/Fan.cs
+++ Project23/UsingAndDisposable/MediatorPattern/HomeMediator/Fan.cs
@@ -7,6 +7,17 @@ namespace UsingAndDisposable.MediatorPattern.HomeMediator
 {
     public class Fan
     {
-        
+        private IHomeMediator _mediator;
+        private bool _isOn;
+        public void SetMediator(IHomeMediator mediator) => _mediator = mediator;
+        public void TurnOn()
+        {
+            _isOn = true;
+        }
+
+        public void TurnOff()
+        {
+            _isOn = false;
+        }
     }
 }
